92a2ea7e429a98903aac971c7207eedfe95f1bfe
jjenk <j.jenkinson89@hotmail.com>
2024-10-25 15:26:18 +0200

    data finished, readme edited

--- src/data/data.ts
+++ src/data/data.ts
@@ -44,4 +44,229 @@ export const data = [
         d: "driver",
       }
     },
+    {
+      name: "4: If I am having a conflict with a co-worker or customer...",
+      answers: {
+        a: "I try to help the situation along by focusing on the positive",
+        b: "I stay calm and try to understand the cause of the conflict",
+        c: "I try to avoid discussing the issue causing the conflict",
+        d: "I confront it right away so that it can get resolved as soon as possible"
+      },
+      values: {
+        a: "expressive",
+        b: "amiable",
+        c: "analytical",
+        d: "driver",
+      }
+    },
+    {
+      name: "5: When I talk on the phone at work...",
+      answers: {
+        a: "I keep the conversation focused on the purpose of the call",
+        b: "I spend a few minutes chatting before getting down to business",
+        c: "I am in no hurry to get off the phone and don't mind chatting about personal things, the weather, and so on",
+        d: "I try to keep the conversation as brief as possible"
+      },
+      values: {
+        a: "driver",
+        b: "expressive",
+        c: "amiable",
+        d: "analytical",
+      }
+    },
+    {
+      name: "6: If a co-worker is upset...",
+      answers: {
+        a: "I ask if I can do anything to help",
+        b: "I leave them alone because I don't want to intrude on their privacy",
+        c: "I try to cheer them up and help them to see the bright side",
+        d: "I feel uncomfortable and hope they get over it soon"
+      },
+      values: {
+        a: "amiable",
+        b: "analytical",
+        c: "expressive",
+        d: "driver",
+      }
+    },
+    {
+      name: "7: When I attend meetings at work...",
+      answers: {
+        a: "I sit back and think about what is being said before offering my opinion",
+        b: "I put all my cards on the table so my opinion is well known",
+        c: "I express my opinion enthusiastically, but listen to other's ideas as well",
+        d: "I try to support the ideas of the other people in the meeting"
+      },
+      values: {
+        a: "analytical",
+        b: "driver",
+        c: "expressive",
+        d: "amiable",
+      }
+    },
+    {
+      name: "8: When I make a presentation in front of a group...",
+      answers: {
+        a: "I am entertaining and often humorous",
+        b: "I am clear and concise",
+        c: "I speak relatively quietly",
+        d: "I am direct, specific, and sometimes loud"
+      },
+      values: {
+        a: "expressive",
+        b: "analytical",
+        c: "amiable",
+        d: "driver",
+      }
+    },
+    {
+      name: "9: When a customer is explaining a problem to me...",
+      answers: {
+        a: "I try to understand and empathise with how they are feeling",
+        b: "I look for the specific facts pertaining to the situation",
+        c: "I listen carefully for the main issue so that I can find a solution",
+        d: "I use my body language and tone of voice to show them that I understand"
+      },
+      values: {
+        a: "amiable",
+        b: "analytical",
+        c: "driver",
+        d: "expressive",
+      }
+    },
+    {
+      name: "10: When I attend training programs or presentations...",
+      answers: {
+        a: "I get bored if the person moves too slowly",
+        b: "I try to be supportive of the speaker, knowing how hard the job is",
+        c: "I want it to be entertaining as well as informative",
+        d: "I look for the logic behind what the speaker is saying"
+      },
+      values: {
+        a: "driver",
+        b: "amiable",
+        c: "expressive",
+        d: "analytical",
+      }
+    },
+    {
+      name: "11: When I want to get my point across to customers or co-workers...",
+      answers: {
+        a: "I listen to their point of view first and then express my ideas gently",
+        b: "I strongly state my opinion so that they know where I stand",
+        c: "I try to persuade them without being too forceful",
+        d: "I explain the thinking and logic behind what I am saying"
+      },
+      values: {
+        a: "amiable",
+        b: "driver",
+        c: "expressive",
+        d: "analytical",
+      }
+    },
+    {
+      name: "12: When I am late for a meeting or appointment...",
+      answers: {
+        a: "I don't panic but call ahead to say that I will be a few minutes late",
+        b: "I feel bad about keeping the other person waiting",
+        c: "I get very upset and rush to get there as soon as possible",
+        d: "I apologise profusely once I arrive"
+      },
+      values: {
+        a: "analytical",
+        b: "amiable",
+        c: "driver",
+        d: "expressive",
+      }
+    },
+    {
+      name: "13: I set goals and objectives at work that...",
+      answers: {
+        a: "I think I can realistically attain",
+        b: "I feel are challenging and would be exciting to achieve",
+        c: "I need to achieve as part of a bigger objective",
+        d: "Will make me feel good when I achieve them"
+      },
+      values: {
+        a: "analytical",
+        b: "expressive",
+        c: "driver",
+        d: "amiable",
+      }
+    },
+    {
+      name: "14: When explaining a problem to a co-worker whom I need help from...",
+      answers: {
+        a: "I explain the problem in as much detail as possible",
+        b: "I sometimes exaggerate to make my point",
+        c: "I try to explain how the problem makes me feel",
+        d: "I explain how I would like the problem to be solved"
+      },
+      values: {
+        a: "analytical",
+        b: "expressive",
+        c: "amiable",
+        d: "driver",
+      }
+    },
+    {
+      name: "15: If customers or co-workers are late for a meeting with me in my office...",
+      answers: {
+        a: "I keep myself busy by making phone calls or working until they arrive",
+        b: "I assume they were delayed a bit and don't get upset",
+        c: "I call to make sure that I have the correct information (date, time, and so on)",
+        d: "I get upset that the person is wasting my time"
+      },
+      values: {
+        a: "expressive",
+        b: "amiable",
+        c: "analytical",
+        d: "driver",
+      }
+    },
+    {
+      name: "16: When I am behind on a project and feel pressure to get it done...",
+      answers: {
+        a: "I make a list of everything I need to do, in what order, by when",
+        b: "I block out everything else and focus 100 percent on the work I need to do",
+        c: "I become anxious and have a hard time focusing on my work",
+        d: "I set a date to get the project done by and go for it"
+      },
+      values: {
+        a: "analytical",
+        b: "driver",
+        c: "amiable",
+        d: "expressive",
+      }
+    },
+    {
+      name: "17: When I feel verbally attacked by a customer or a co-worker...",
+      answers: {
+        a: "I tell them to stop it",
+        b: "I feel hurt but usually don't say anything about it to them",
+        c: "I ignore their anger and try to focus on the facts of the situation",
+        d: "I let them know in strong terms that I don't like their behaviour"
+      },
+      values: {
+        a: "driver",
+        b: "amiable",
+        c: "analytical",
+        d: "expressive",
+      }
+    },
+    {
+      name: "18: When I see a co-worker or customer whom I like and haven't seen recently...",
+      answers: {
+        a: "I give them a friendly hug",
+        b: "I greet them but don't shake their hand.",
+        c: "I give them a firm but quick handshake",
+        d: "I give them an enthusiastic handshake that lasts a few moments"
+      },
+      values: {
+        a: "amiable",
+        b: "analytical",
+        c: "driver",
+        d: "expressive",
+      }
+    },
   ]
